ed3c40813fbca41192d16030cfb8a9912d7f965f
dipankrr <dipankar.laptop@gmail.com>
2025-06-14 23:58:33 +0530

    express app connect

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import app from "./app.js"
 
 dotenv.config();
 
+//const port = process.env.PORT || 8000
+
+
+
 connectDB()
-
-
+.then(() => {
+    app.listen( process.env.PORT, () => {
+        console.log(`Express server running on port: ${process.env.PORT}`)
+    })
+    
+    // app.on("error", (error) => {
+    //         console.log("ERORR", error);
+    //         throw error
+    // })
+    
+})
+.catch((error) => {
+    console.log("Express connecting error", error)
+})
 
 
 
